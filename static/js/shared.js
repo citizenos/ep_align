8246e86609f50b7bf51cbfcaf9266772f1f9ec3f
ilmar <ilmar.tyrk@gmail.com>
2020-05-30 00:45:19 +0300

    Update in sync with original

--- static/js/shared.js
+++ static/js/shared.js
@@ -7,17 +7,20 @@ var collectContentPre = function(hook, context){
   var state = context.state;
   var lineAttributes = state.lineAttributes
   var tagIndex = _.indexOf(tags, tname);
+  if(tname === "div" || tname === "p"){
+    delete lineAttributes['align'];
+  }
   if(tagIndex >= 0){
     lineAttributes['align'] = tags[tagIndex];
   }
 };
 
+// I don't even know when this is run..
 var collectContentPost = function(hook, context){
   var tname = context.tname;
   var state = context.state;
   var lineAttributes = state.lineAttributes
   var tagIndex = _.indexOf(tags, tname);
-
   if(tagIndex >= 0){
     delete lineAttributes['align'];
   }
